3e4d71910482fd27e283984afe52fbb9b48cfd87
Barry <75135224+rmd926@users.noreply.github.com>
2025-06-24 15:17:16 +0800

    Add files via upload

---
+++ APCS/zerojudge/c294.py
@@ -0,0 +1,13 @@
+a = [int(i) for i in input().split()]
+
+a.sort()
+print(*a)
+
+if a[0]+a[1] <= a[2]:
+    print('No')
+elif a[0]**2 + a[1]**2 == a[2]**2:
+    print('Right')
+elif a[0]**2 + a[1]**2 > a[2]**2:
+    print('Acute')
+else:
+    print('Obtuse')
