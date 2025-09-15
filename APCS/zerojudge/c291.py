f2114e1ca627859b3d5e664f6b56afc100e1e9ef
Barry <75135224+rmd926@users.noreply.github.com>
2025-09-15 22:47:12 +0800

    Update c291.py

--- APCS/zerojudge/c291.py
+++ APCS/zerojudge/c291.py
@@ -1,32 +1,4 @@
-# n = int(input())
-# # self_list = [int(x) for x in range(0, n, 1)]
-
-# friends_list = [int(i) for i in input().split()]
-# visit_flag = [False] * n # record the status of visit
-# count = 0
-
-# for i in range(n):
-#     if visit_flag[i] == True: # continue
-#         continue
-#     else: #doesn't visit yet
-#         visit_flag[i] = True
-#         pointer = friends_list[i]
-#         count += 1
-#     while pointer != i: #只要指標還不是指到該群組的first num
-#         visit_flag[pointer] = True
-#         pointer = friends_list[pointer] #pointer繼續更新成friends_list對應到的數字
-
-# print(count)
-
-
-
-
-
-
-
-
-
-
+# https://zerojudge.tw/ShowProblem?problemid=c291
 
 n = int(input())
 friends_list = [int(x) for x in input().split()]
@@ -47,6 +19,7 @@ for i in range(n):
         pair_num = friends_list[pair_num] # 再把i配到的那個數字更新成那個數字該對應的數字
 
 print(count)
+
 
 
 
