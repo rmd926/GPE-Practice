f7ca94fa6bc45b986acfb8f173ff537d024995bc
Barry <75135224+rmd926@users.noreply.github.com>
2025-09-16 19:07:11 +0800

    Add files via upload

---
+++ APCS/zerojudge/m931.py
@@ -0,0 +1,19 @@
+# https://zerojudge.tw/ShowProblem?problemid=m931
+n = int(input())
+pairs = []
+temp = []
+
+for _ in range(n):
+    a, d = map(int, input().split())
+    pairs.append((a,d))
+
+temp = [a*a + d*d for a, d in pairs]
+target = sorted(temp)[-2]
+
+for a,d in pairs:
+    if a**2 + d**2 == target:
+        print(a, d)
+        break
+
+
+
