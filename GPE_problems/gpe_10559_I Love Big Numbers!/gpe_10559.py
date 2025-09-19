05ba2981103bd0d79c044d7ec5ae30d5c1898fb8
Barry <75135224+rmd926@users.noreply.github.com>
2025-09-20 02:52:30 +0800

    Add files via upload

---
+++ GPE_problems/gpe_10559_I Love Big Numbers!/gpe_10559.py
@@ -0,0 +1,43 @@
+# import sys
+# def factorial(n: int) -> int:
+#     fac = [1] * (n + 1)      # fac[0] = 1
+#     for i in range(1, n + 1):
+#         fac[i] = fac[i - 1] * i
+#     return fac[n]
+
+# target = int(sys.stdin.readline())
+# while True:
+#     ans = 0
+#     for num in str(factorial(target)):
+#         ans += int(num)
+#     print(ans)
+#  TLE ==
+
+import sys
+
+def factorial(n: int):
+    fac = [1] * (n + 1)      # fac[0] = 1
+    for i in range(1, n + 1):
+        fac[i] = fac[i - 1] * i
+    return fac
+def main():
+
+    nums = list(map(int, sys.stdin.read().split()))
+    if not nums:
+        sys.exit(0)
+
+    max_n = max(nums)
+    fac = factorial(max_n)
+    
+
+    for n in nums:
+        total = 0
+        for i in str(fac[n]):
+            total += int(i)
+        print(total)
+
+if __name__ == '__main__':
+    main()
+        # print(sum(int(d) for d in str(fac[n])))
+
+    
