3d61d9b943958dcc0040e1d31909155e9c5d18fa
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-09 04:05:11 +0800

    Add files via upload

---
+++ CPE/uva_10346 Peter's Smokes.py
@@ -0,0 +1,16 @@
+import sys
+
+nums = list(map(int, sys.stdin.read().split()))
+
+for i in range(0, len(nums) - 1, 2):
+    n = nums[i]
+    k = nums[i + 1]
+    ans = n
+
+    while n >= k:
+        
+        remain = n % k
+        ans += n//k
+        n = n//k + remain
+        
+    print(ans)
