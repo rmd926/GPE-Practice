5dd40114bf83b3fbec86117e472af0f42aadb0ea
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-22 17:12:36 +0800

    Add files via upload

---
+++ CPE/uva_10539 Almost Prime Numbers.py
@@ -0,0 +1,26 @@
+from bisect import bisect_left, bisect_right
+
+MAX = 10 ** 12
+is_prime = [True] * int(MAX**0.5+1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(MAX**0.5+1)):
+    if is_prime[i]:
+        for j in range(i**2, int(MAX**0.5+1), i):
+            is_prime[j] = False
+
+# almost_prime其實就是prime的n次方
+almost_prime = []
+for i in range(2, int(MAX**0.5+1)):
+    if is_prime[i]:
+        x = i**2
+        while x <= MAX:
+            almost_prime.append(x)
+            x *= i
+
+almost_prime.sort()
+
+tc = int(input())
+for _ in range(tc):
+    low, high = map(int, input().split())
+    print(bisect_right(almost_prime, high) - bisect_left(almost_prime, low)) # binary search
