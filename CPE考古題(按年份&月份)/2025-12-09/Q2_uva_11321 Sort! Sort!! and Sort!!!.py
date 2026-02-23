82a8c734c094d91857e2776f8672db8d0dca1345
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-23 15:16:37 +0800

    Create Q2_uva_11321 Sort! Sort!! and Sort!!!.py

---
+++ CPE考古題(按年份&月份)/2025-12-09/Q2_uva_11321 Sort! Sort!! and Sort!!!.py
@@ -0,0 +1,25 @@
+def mod(x, m):
+    # C-style mod (division truncates toward 0), remainder may be negative
+    return x - int(x / m) * m
+
+while True:
+    n, m = map(int, input().split())
+    print(n, m)
+    if n == 0 and m == 0:
+        break
+
+    nums = []
+    for _ in range(n):
+        nums.append(int(input()))
+
+    def key(x):
+        r = mod(x, m)
+        if x % 2 != 0:          # odd
+            return (r, 0, -x)   # odd first, larger first
+        else:                   # even
+            return (r, 1, x)    # even later, smaller first
+
+    nums.sort(key=key)
+
+    for x in nums:
+        print(x)
