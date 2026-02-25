38f1647753a843ffdec537bb42edc53307e002fb
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-26 01:42:02 +0800

    Create Q6_uva_941 Permutations.py

---
+++ CPE考古題(按年份&月份)/2024-05-21/Q6_uva_941 Permutations.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+tc = int(input())
+for _ in range(tc):
+	target = sorted(input())
+	n = int(input())
+	ans = ""
+	
+	while len(target) > 0:
+		x = factorial(len(target)-1)
+		i = n // x # 先算i落在第幾個區塊，然後把target[i]加到ans去之後，再繼續做
+		ans += target[i]
+		target = target[:i] + target[i+1:]
+		n %= x
+		
+	print(ans)
