0ac016ace59667a7fb3d20a8920257f28d692b11
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-27 02:02:09 +0800

    Create Q4_uva_11240 Antimonotonicity.py

---
+++ CPE考古題(按年份&月份)/2023-03-21/Q4_uva_11240 Antimonotonicity.py
@@ -0,0 +1,10 @@
+tc = int(input())
+for _ in range(tc):
+	target = list(map(int, input().split()))[1:]
+	length = 1
+	
+	for i in range(1, len(target)):
+		if (length % 2 == 1 and target[i] < target[i-1]) or (length % 2 == 0 and target[i] > target[i-1]):
+			length += 1
+	
+	print(length)
