71d8e9821f7f26e40a38af1d448509ce1028383f
Barry <75135224+rmd926@users.noreply.github.com>
2026-02-27 18:24:09 +0800

    Create Q2_uva_11349 Symmetric Matrix.py

---
+++ CPE考古題(按年份&月份)/2023-05-23/Q2_uva_11349 Symmetric Matrix.py
@@ -0,0 +1,21 @@
+tc = int(input())
+
+for t in range(tc):
+	size = int(input().split()[-1])
+	M = [[int(num) for num in input().split()] for _ in range(size)]
+	status = True
+	
+	for i in range(size):
+		for j in range(size):
+			n1 = M[i][j]
+			n2 = M[size-i-1][size-j-1]
+			
+			if n1 != n2 or n1 < 0 or n2 < 0:
+				status = False
+				break
+			
+	if status:
+		print(f"Test #{t+1}: Symmetric.")
+	else:
+		print(f"Test #{t+1}: Non-symmetric.")
+				
