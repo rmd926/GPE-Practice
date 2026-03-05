eff25427da07b7002a44cc60069e7c9f38219c67
Barry <75135224+rmd926@users.noreply.github.com>
2026-03-05 23:20:09 +0800

    Create Q43_uva_10420 List of Conquests.py

---
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q43_uva_10420 List of Conquests.py
@@ -0,0 +1,13 @@
+tc = int(input())
+lookup = {}
+
+for _ in range(tc):
+	target = list(map(str, input().split()))
+	country = target[0]
+	if country not in lookup:
+		lookup[country] = 1
+	else:
+		lookup[country] += 1
+	
+for key, value in sorted(lookup.items()):
+	print(key, value)
