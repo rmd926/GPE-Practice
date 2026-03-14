781582ba6256c1d4e784e00cb264e9ec0bff5c74
Barry <75135224+rmd926@users.noreply.github.com>
2026-03-14 22:30:07 +0800

    Create Q4_uva_406 Prime Cuts.py

---
+++ CPE考古題(按年份&月份)/2016-03-22/Q4_uva_406 Prime Cuts.py
@@ -0,0 +1,34 @@
+MAX = 1000
+is_prime = [True] * (MAX + 1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(MAX ** 0.5) + 1):
+    if is_prime[i]:
+        for j in range(i * i, MAX + 1, i):
+            is_prime[j] = False
+
+while True:
+    N, C = map(int, input().split())
+    ans = [1]
+    
+    for num in range(2, N + 1):
+        if is_prime[num]:
+            ans.append(num)
+
+    n = len(ans)
+    mid = n // 2
+
+    if n % 2 == 0:
+        if 2 * C >= n:
+            output = ans
+        else:
+            output = ans[mid - C : mid + C]
+    else:
+        if 2 * C - 1 >= n:
+            output = ans
+        else:
+            output = ans[mid - (C - 1) : mid + C]
+
+    print(f"{N} {C}: ", end="")
+    print(*output)
+    print()
