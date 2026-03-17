cba1bcdc5d9125607e124157c0f732800b80e457
Barry <75135224+rmd926@users.noreply.github.com>
2026-03-17 19:45:33 +0800

    Create Q2_uva_10663 RareEasyProblem.py

---
+++ CPE考古題(按年份&月份)/2018-03-27/Q2_uva_10663 RareEasyProblem.py
@@ -0,0 +1,12 @@
+while True:
+	target = int(input())
+	if target == 0:
+		break
+	
+	ans = []
+	ans.append(target + target // 9)
+	if target % 9 == 0:
+		ans.append(target + target // 9 - 1)
+	ans.sort()
+	
+	print(*ans)
