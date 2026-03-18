a10142e8e89f0abeaa6feb1b2eb66bb237c5a2da
Barry <75135224+rmd926@users.noreply.github.com>
2026-03-18 20:08:09 +0800

    Create Q2_uva_10922 2 the 9s.py

---
+++ CPE考古題(按年份&月份)/2015-03-24/Q2_uva_10922 2 the 9s.py
@@ -0,0 +1,24 @@
+while True:
+	target = int(input())
+	if target == 0:
+		break
+	
+	if target == 9:
+		print("9 is a multiple of 9 and has 9-degree 1.")
+	
+	elif target % 9 != 0:
+		print(f"{target} is not a multiple of 9.")
+
+	
+	else:
+		new_num = target
+		count = 0
+		
+		while new_num != 9:
+			count += 1
+			temp = 0
+			for char in str(new_num):
+				temp += int(char)
+			new_num = temp
+		
+		print(f"{target} is a multiple of 9 and has 9-degree {count}.")a
