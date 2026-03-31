3cafbbd5870e9d81546128d6ad5f5eddc19f77a8
Barry <75135224+rmd926@users.noreply.github.com>
2026-04-01 00:34:49 +0800

    Update uva_11059 Maximum Product.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11059 Maximum Product.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11059 Maximum Product.py
@@ -1,21 +1,27 @@
 tc = 1
 while True:
-	line = input()
-	if line == " ":
-		continue
-	
-	n = int(line)
-	target = list(map(int, input().split()))
-	ans = 0
-	
-	for i in range(n):
-		temp = target[i]
-		for j in range(i+1, n):
-			temp *= target[j]
-			if temp > ans:
-				ans = temp
-	
-	print(f"Case #{tc}: The maximum product is {ans}.")
-	print()
-	
-	tc += 1
+    try:
+        line = input()
+    except:
+        break
+
+    if line == "":
+        continue
+
+    n = int(line)
+    target = list(map(int, input().split()))
+    ans = 0
+
+    for i in range(n):
+        temp = target[i]
+        if temp > ans:
+            ans = temp
+        for j in range(i + 1, n):
+            temp *= target[j]
+            if temp > ans:
+                ans = temp
+
+    print(f"Case #{tc}: The maximum product is {ans}.")
+    print()
+
+    tc += 1
