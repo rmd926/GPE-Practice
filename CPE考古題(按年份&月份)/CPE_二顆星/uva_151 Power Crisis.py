86b6af8987ada7aa44b3eb4153aab6bbc70e2423
Barry <75135224+rmd926@users.noreply.github.com>
2026-04-05 00:05:17 +0800

    Create uva_151 Power Crisis.py

---
+++ CPE考古題(按年份&月份)/CPE_二顆星/uva_151 Power Crisis.py
@@ -0,0 +1,25 @@
+while True:
+	try:
+		n = int(input())
+	except:
+		break
+
+	if n == 0:
+		break
+	
+	step = 1
+
+	while True:
+		target = [int(x) for x in range(1, n+1)]
+		target.pop(0)
+		idx = 0
+
+		while len(target) > 1:
+			idx = (idx + step - 1) % len(target)
+			target.pop(idx)
+
+		if target[0] == 13:
+			print(step)
+			break
+
+		step += 1
