a6577e62e3c4ca86778aee4d248ca390247c5c5e
Barry <75135224+rmd926@users.noreply.github.com>
2026-04-23 20:21:24 +0800

    Create uva_13171 Pixel Art.py

---
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_13171 Pixel Art.py
@@ -0,0 +1,41 @@
+tc = int(input())
+
+for _ in range(tc):
+    m, y, c, s = input().split()
+    m = int(m)
+    y = int(y)
+    c = int(c)
+
+    status = True
+
+    for ch in s:
+        if ch == 'M':
+            m -= 1
+        elif ch == 'Y':
+            y -= 1
+        elif ch == 'C':
+            c -= 1
+        elif ch == 'R':
+            m -= 1
+            y -= 1
+        elif ch == 'G':
+            y -= 1
+            c -= 1
+        elif ch == 'V':
+            m -= 1
+            c -= 1
+        elif ch == 'B':
+            m -= 1
+            y -= 1
+            c -= 1
+        elif ch == 'W':
+            pass
+
+        if m < 0 or y < 0 or c < 0:
+            status = False
+            break
+
+    if status:
+        print("YES", m, y, c)
+    else:
+        print("NO")
