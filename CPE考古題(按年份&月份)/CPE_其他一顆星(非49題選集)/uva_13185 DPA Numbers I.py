00fe02234d3d5183162368fb58e0e55fc06bee64
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-09 17:00:44 +0800

    Update uva_13185 DPA Numbers I.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_13185 DPA Numbers I.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_13185 DPA Numbers I.py
@@ -1,23 +1,18 @@
-def factor_sum(target: int):
-    ans = 0
-    for num in range(1, target//2 + 1):
-        if target % num == 0:
-            ans += num
-
-    return ans
+def factor_sum(target):
+	ans = 0
+	for num in range(1, target//2 + 1):
+		if target % num == 0:
+			ans += num
+	return ans
 
 tc = int(input())
 for _ in range(tc):
-    try:
-        target = int(input())
-    except:
-        break
-
-    if target == factor_sum(target):
-        print("perfect")
-    
-    elif target > factor_sum(target):
-        print("deficient")
-    
-    else:
-        print("abundant")
+	target = int(input())
+	if factor_sum(target) > target:
+		print("abundant")
+		
+	elif factor_sum(target) < target:
+		print("deficient")
+	
+	else:
+		print("perfect")
