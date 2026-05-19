2d3d2d35f7b2693dffb3a3eddf775108c7a8d554
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-19 16:26:29 +0800

    Update Q02_uva_10783 Odd Sum.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q02_uva_10783 Odd Sum.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q02_uva_10783 Odd Sum.py
@@ -1,13 +1,14 @@
 tc = int(input())
 for t in range(tc):
-	a = int(input())
-	b = int(input())
+	lower = int(input())
+	upper = int(input())
 	ans = 0
 	
-	if a % 2 == 0:
-		a += 1
+	if lower % 2 == 0:
+		lower += 1
 	
-	for odd in range(a, b+1, 2):
-		ans += odd
-	
+	for num in range(lower, upper+1, 2):
+		ans += num
+		
 	print(f"Case {t+1}: {ans}")
+# 2026.05.19 二刷
