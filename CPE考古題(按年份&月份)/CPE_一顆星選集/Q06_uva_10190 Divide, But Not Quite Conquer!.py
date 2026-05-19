6e184bb6cbbc3986ad44b880678c840ce0d6b841
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-19 16:53:50 +0800

    Update Q06_uva_10190 Divide, But Not Quite Conquer!.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q06_uva_10190 Divide, But Not Quite Conquer!.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q06_uva_10190 Divide, But Not Quite Conquer!.py
@@ -20,3 +20,24 @@ while True:
 		
 		else:
 			print("Boring!")	
+# version 1
+
+while True:
+	target, k = map(int, input().split())
+	
+	if target == 0 or k == 0 or target == 1 or k == 1:
+		print("Boring!")
+		
+	else:
+		ans = [target]
+		
+		while target % k == 0:
+			ans.append(target // k)
+			target //= k
+		
+		if target == 1:
+			print(*ans)
+		else:
+			print("Boring!")
+
+# 2026.05.19 二刷
