36334975d77a36c7af520eee1bcac2914552248f
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-20 01:37:55 +0800

    Update Q11_uva_10035 Primary Arithmetic.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q11_uva_10035 Primary Arithmetic.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q11_uva_10035 Primary Arithmetic.py
@@ -2,14 +2,14 @@ while True:
 	a, b = map(int, input().split())
 	if a == b == 0:
 		break
+	
 	carry = 0
 	ans = 0
 	
 	while a > 0 or b > 0:
 		if a % 10 + b % 10 + carry >= 10:
-			ans += 1
 			carry = 1
-			
+			ans += 1
 		else:
 			carry = 0
 		
@@ -18,9 +18,11 @@ while True:
 	
 	if ans == 0:
 		print("No carry operation.")
-	
+		
 	elif ans == 1:
 		print("1 carry operation.")
-	
+		
 	else:
 		print(f"{ans} carry operations.")
+
+# 2026.05.20 二刷
