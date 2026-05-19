0c82a06e90a5ade1c5b032ffa075150bc3bd7fca
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-19 16:45:47 +0800

    Update Q05_uva_11150 Cola.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q05_uva_11150 Cola.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q05_uva_11150 Cola.py
@@ -3,12 +3,14 @@ while True:
 	ans = target
 	
 	while target >= 3:
-		temp = (target // 3)
-		ans += temp
+		temp = target // 3
 		remain = target % 3
+		ans += temp
 		target = temp + remain
 	
 	if target == 2:
 		ans += 1
 	
 	print(ans)
+
+# 2026.05.19
