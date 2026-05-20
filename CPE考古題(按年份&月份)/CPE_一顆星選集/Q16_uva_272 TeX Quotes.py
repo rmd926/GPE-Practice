1fc3589c34ce2441bfdb128081a8b58f791fbf6d
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-20 20:13:47 +0800

    Update Q16_uva_272 TeX Quotes.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q16_uva_272 TeX Quotes.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q16_uva_272 TeX Quotes.py
@@ -1,15 +1,18 @@
 count = 0
 while True:
 	target = input()
-	for char in target:
-		if char == '"':
-			count += 1
-			if count % 2 == 1:
-				print("``", end = "")
-			else:
-				print("''", end = "")
-		
-		else:
-			print(char, end = "")
+	ans = ""
 	
-	print()
+	for ch in target:
+		if ch == '"':
+			count += 1
+			
+			if count % 2 == 1:
+				ans += "``"
+			else:
+				ans += "''"
+		else:
+			ans += ch
+	
+	print(ans)
+# 2026.05.20 二刷 要留意count初始化的位置
