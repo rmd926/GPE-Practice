1fe05455d43b1eb3adecc5998eb2952fc4589785
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-21 00:35:44 +0800

    Update Q32_uva_10008 What's Cryptanalysis?.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q32_uva_10008 What's Cryptanalysis?.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q32_uva_10008 What's Cryptanalysis?.py
@@ -1,13 +1,16 @@
-tc = int(input())
 lookup = {}
+tc = int(input())
 for _ in range(tc):
-	line = input().upper()
-	for char in line:
-		if "A" <= char <= "Z":
-			if char not in lookup:
-				lookup[char] = 1
+	target = input().upper()
+	
+	for ch in target:
+		if ch.isalpha():
+			if ch not in lookup:
+				lookup[ch] = 1
 			else:
-				lookup[char] += 1
-
+				lookup[ch] += 1
+	
 for key, value in sorted(lookup.items(), key = lambda x: (-x[1], x[0])):
 	print(key, value)
+
+# 2026.05.21 二刷
