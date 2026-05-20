772f168da62e134aa72878588643718ffe203f13
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-20 19:53:23 +0800

    Update Q14_uva_10222 Decode the Mad man.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q14_uva_10222 Decode the Mad man.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q14_uva_10222 Decode the Mad man.py
@@ -1,12 +1,13 @@
 keyboard = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"
-tc = int(input())
 
-for _ in range(tc):
-    target = input()
-    
-    for char in target.lower():
-        if char == " ":
-            print(" ", end="")
-        else:
-            print(keyboard[keyboard.index(char) - 2], end="")
-    print()
+while True:
+	target = input()
+	ans = ""
+	
+	for ch in target.lower():
+		if ch in keyboard:
+			ans += keyboard[keyboard.index(ch)-2]
+		else:
+			ans += ch
+	print(ans)
+# 2026.05.20 二刷
