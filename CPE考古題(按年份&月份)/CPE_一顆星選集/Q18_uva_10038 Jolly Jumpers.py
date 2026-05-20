44ab49b8d3a9be49bda1207aca718c2ffd49c058
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-20 20:24:03 +0800

    Update Q18_uva_10038 Jolly Jumpers.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q18_uva_10038 Jolly Jumpers.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q18_uva_10038 Jolly Jumpers.py
@@ -1,15 +1,17 @@
 while True:
-	target = list(map(int, input().split()))
-	n = target[0]
-	nums = target[1:]
-	check = []
+	seq = list(map(int, input().split()))
+	n = seq[0]
+	target = seq[1:]
+	temp = []
+	
 	gt = [int(x) for x in range(1, n)]
-	
 	for i in range(1, n):
-		check.append(abs(nums[i-1]-nums[i]))
+		temp.append(abs(target[i] - target[i-1]))
 	
-	if sorted(check) == gt:
+	if sorted(temp) == gt:
 		print("Jolly")
-	
+		
 	else:
 		print("Not jolly")
+
+# 2026.05.20 二刷
