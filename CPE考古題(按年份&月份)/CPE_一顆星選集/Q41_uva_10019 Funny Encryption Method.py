fa9324f0d9d64747fdc3a5e1889c97fc1e4864b8
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 02:19:46 +0800

    Update Q41_uva_10019 Funny Encryption Method.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q41_uva_10019 Funny Encryption Method.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q41_uva_10019 Funny Encryption Method.py
@@ -5,14 +5,16 @@ for _ in range(tc):
 	
 	X1 = bin(target)[2:]
 	X2 = bin(hex_target)[2:]
-	ans1, ans2 = 0, 0
 	
-	for char in X1:
-		if char == "1":
+	ans1, ans2 = 0, 0
+	for ch in X1:
+		if ch == "1":
 			ans1 += 1
 	
-	for char in X2:
-		if char == "1":
+	for ch in X2:
+		if ch == "1":
 			ans2 += 1
-	
+
 	print(ans1, ans2)
+
+# 2026.05.23 二刷 要看清楚題意，int(x, base=10) x需要是字串或者是數字
