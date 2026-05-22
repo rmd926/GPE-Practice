6a177fefc39a36473494b94a763c7367f620ad0c
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 00:50:52 +0800

    Update Q28_uva_10235 Simply Emirp.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q28_uva_10235 Simply Emirp.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q28_uva_10235 Simply Emirp.py
@@ -1,21 +1,22 @@
 MAX = 10 ** 6
 is_prime = [True] * (MAX+1)
 is_prime[0] = is_prime[1] = False
-for i in range(2, int(MAX**0.5)+1):
+for i in range(2, int(MAX**0.5) + 1):
 	if is_prime[i]:
 		for j in range(i**2, MAX+1, i):
 			is_prime[j] = False
 
 while True:
 	target = input()
-	if int(target) == int(target[::-1]) and is_prime[int(target)] and is_prime[int(target[::-1])]:
+	if target == target[::-1] and is_prime[int(target)]:
 		print(f"{target} is prime.")
-		
-	elif is_prime[int(target)] and is_prime[int(target[::-1])]:
-		print(f"{target} is emirp.")
-		
 	elif is_prime[int(target)] and not is_prime[int(target[::-1])]:
 		print(f"{target} is prime.")
 		
 	elif not is_prime[int(target)]:
 		print(f"{target} is not prime.")
+	
+	elif target != target[::-1] and is_prime[int(target)] and is_prime[int(target[::-1])]:
+		print(f"{target} is emirp.")
+
+# 2026.05.23 二刷 要注意條件判斷式，例如11這種case要判斷成prime而不是emirp
