388bb7f50b320ddcb6ff2017c0729d365cecf864
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-22 23:18:09 +0800

    Update Q21_uva_10922 2 the 9s.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q21_uva_10922 2 the 9s.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q21_uva_10922 2 the 9s.py
@@ -2,22 +2,22 @@ while True:
 	target = input()
 	if target == "0":
 		break
-	
 	elif target == "9":
 		print("9 is a multiple of 9 and has 9-degree 1.")
 	
 	elif int(target) % 9 == 0:
-		new_num = target
+		cur_num = target
 		count = 0
 		
-		while int(new_num) != 9:
+		while int(cur_num) != 9:
 			count += 1
 			temp = 0
-			for char in new_num:
-				temp += int(char)
-			new_num = str(temp)
-			
+			for ch in cur_num:
+				temp += int(ch)
+			cur_num = str(temp)
 		print(f"{target} is a multiple of 9 and has 9-degree {count}.")
 	
 	else:
 		print(f"{target} is not a multiple of 9.")
+
+# 2026.05.22 二刷 要注意% 9那邊 count怎麼加
