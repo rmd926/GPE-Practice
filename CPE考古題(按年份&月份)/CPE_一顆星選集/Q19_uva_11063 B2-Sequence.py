25bacfb8bc298140c792ffd632581a5ed88ab263
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-22 15:08:00 +0800

    Update Q19_uva_11063 B2-Sequence.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q19_uva_11063 B2-Sequence.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q19_uva_11063 B2-Sequence.py
@@ -2,27 +2,28 @@ tc = 1
 while True:
 	n = int(input())
 	target = list(map(int, input().split()))
+	
 	status = True
 	seq = []
-	
-	for i in range(n-1):
-		for j in range(i+1, n):
-			if target != sorted(target) or 0 in target:
+	for i in range(1, n):
+		if target != sorted(target) or 0 in target:
+			status = False
+			break
+			
+		else:
+			if abs(target[i]-target[i-1]) not in seq:
+				seq.append(abs(target[i]-target[i-1]))
+			else:
 				status = False
 				break
-			else:
-				if target[i] + target[j] in seq:
-					status = False
-					break
-				else:
-					seq.append(target[i] + target[j])
-					
+				
 	if status:
 		print(f"Case #{tc}: It is a B2-Sequence.")
-	
 	else:
 		print(f"Case #{tc}: It is not a B2-Sequence.")
-		
-	print()
+	
 	tc += 1
+	print()
 	space = input()
+	
+# 2026.05.22 二刷 要留意格式
