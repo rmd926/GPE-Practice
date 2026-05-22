b3807c71625850cda94ec00cbdfb3942247148ac
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 00:05:52 +0800

    Update Q26_uva_10057 A mid-summer nights dream.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q26_uva_10057 A mid-summer nights dream.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q26_uva_10057 A mid-summer nights dream.py
@@ -1,18 +1,21 @@
 while True:
 	n = int(input())
 	nums = []
-	
 	for _ in range(n):
-		nums.append(int(input()))
+		target = int(input())
+		nums.append(target)
+	
 	nums.sort()
+	mid_left = nums[(n-1) // 2]
+	mid_right = nums[n // 2]
 	
-	mid_left = nums[(n-1)//2]
-	mid_right = nums[n//2]
 	count = 0
-	
-	for i in range(len(nums)):
-		if nums[i] == mid_left or nums[i] == mid_right:
+	for num in nums:
+		if num == mid_left or num == mid_right:
 			count += 1
-		case = mid_right - mid_left + 1
 	
-	print(mid_left, count, case)
+	case = mid_right - mid_left + 1
+	print(f"{mid_left} {count} {case}")
+
+# 2026.05.23 二刷 要小心mid_left、mid_right，不要算錯。
+# hints: 當n是奇數的時候，mid_left = mid_right
