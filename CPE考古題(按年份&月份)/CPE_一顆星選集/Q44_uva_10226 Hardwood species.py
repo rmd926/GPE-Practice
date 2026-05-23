bce37c579fbe0ce38b8f9f2922711dd18eb70438
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 17:23:15 +0800

    Update Q44_uva_10226 Hardwood species.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q44_uva_10226 Hardwood species.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q44_uva_10226 Hardwood species.py
@@ -1,27 +1,28 @@
 tc = int(input())
-input()
+space = input()
 
 for t in range(tc):
-    total = 0
-    lookup = {}
-
-    while True:
-        try:
-            target = input()
-        except EOFError:
-            break
-
-        if target == "":
-            break
-
-        if target not in lookup:
-            lookup[target] = 1
-        else:
-            lookup[target] += 1
-        total += 1
-
-    for key, value in sorted(lookup.items()):
-        print(f"{key} {value * 100 / total:.4f}")
-
-    if t != tc - 1:
-        print()
+	lookup = {}
+	total = 0
+	
+	while True:
+		try:
+			target = input()
+		except:
+			break
+		
+		if target == "":
+			break
+		
+		if target not in lookup:
+			lookup[target] = 1
+		else:
+			lookup[target] += 1
+		total += 1
+			
+	for key, value in sorted(lookup.items()):
+		print(f"{key} {(value * 100 / total):.4f}")
+	
+	if t != tc-1:
+		print()
+# 2026.05.23 二刷，三月份烤過的
