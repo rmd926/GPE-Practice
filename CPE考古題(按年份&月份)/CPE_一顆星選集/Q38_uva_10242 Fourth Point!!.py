1fc838e0d42c9af49e27916d321778507ab786d7
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 16:43:40 +0800

    Update Q38_uva_10242 Fourth Point!!.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q38_uva_10242 Fourth Point!!.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q38_uva_10242 Fourth Point!!.py
@@ -1,15 +1,20 @@
 while True:
 	target = list(map(float, input().split()))
 	point = []
+	
 	for i in range(0, 8, 2):
 		point.append([target[i], target[i+1]])
+	
 	dup = [0, 0]
 	for i in range(4):
 		if point.count(point[i]) == 2:
 			dup = point[i]
+			
 			point.remove(dup)
 			point.remove(dup)
 			break
 	
 	ans = [point[0][0] + point[1][0] - dup[0], point[0][1] + point[1][1] - dup[1]]
 	print(f"{ans[0]:.3f} {ans[1]:.3f}")
+
+# 2026.05.23 二刷 要注意dup那邊，remove不要忘記
