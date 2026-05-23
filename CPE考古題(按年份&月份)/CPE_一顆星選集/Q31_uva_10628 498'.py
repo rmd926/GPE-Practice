d9f60d8c5c0cd90ea044f9fdb10bf05e3ea62a79
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-23 14:00:10 +0800

    Update Q31_uva_10628 498'.py

--- CPE考古題(按年份&月份)/CPE_一顆星選集/Q31_uva_10628 498'.py
+++ CPE考古題(按年份&月份)/CPE_一顆星選集/Q31_uva_10628 498'.py
@@ -1,8 +1,10 @@
 while True:
 	x = int(input())
 	target = list(map(int, input().split()))
+	
 	ans = 0
-	for i in range(len(target)):
-		ans += (len(target)-i-1) * target[i] * x ** max(len(target)-i-2, 0)
-		
+	n = len(target)
+	for i in range(n):
+		ans += (n-i-1) * target[i] * x ** max((n-i-2), 0)
 	print(ans)
+# 2026.05.23 二刷
