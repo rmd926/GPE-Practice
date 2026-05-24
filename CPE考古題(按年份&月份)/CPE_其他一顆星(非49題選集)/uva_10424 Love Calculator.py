7a199f782e2ff39c48aefe8de222da9af311b72f
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 02:18:24 +0800

    Update uva_10424 Love Calculator.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10424 Love Calculator.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10424 Love Calculator.py
@@ -1,35 +1,33 @@
-def calc(name):
-    total = 0
-    
-    for ch in name:
-        if 'a' <= ch <= 'z':
-            total += ord(ch) - ord('a') + 1
-        elif 'A' <= ch <= 'Z':
-            total += ord(ch) - ord('A') + 1
-    
-    while total >= 10:
-        s = 0
-        while total > 0:
-            s += total % 10
-            total //= 10
-        total = s
-    
-    return total
-
+def sol(target):
+	total = 0
+	for ch in target:
+		if "A" <= ch <= "Z":
+			total += ord(ch) - ord("A") + 1
+		elif "a" <= ch <= "z":
+			total += ord(ch) - ord("a") + 1
+	while total >= 10:
+		temp = 0
+		for ch in str(total):
+			temp += int(ch)
+		total = temp
+		
+	return total
+	
 while True:
-    try:
-        name1 = input()
-        name2 = input()
-    except EOFError:
-        break
-    
-    n1 = calc(name1)
-    n2 = calc(name2)
-    
-    if n1 > n2:
-        n1, n2 = n2, n1
-    
-    if n2 == 0:
-        print("0.00 %")
-    else:
-        print(f"{n1 * 100 / n2:.2f} %")
+	try:
+		s1 = input()
+		s2 = input()
+	except:
+		break
+	
+	n1 = sol(s1)
+	n2 = sol(s2)
+	
+	if n1 > n2:
+		n1, n2 = n2, n1
+	
+	if n2 == 0:
+		print("0.00 %")
+	else:
+		print(f"{(100 * n1 / n2):.2f} %")
+# 2026.05.25 二刷 細節要注意尤其是分母為0
