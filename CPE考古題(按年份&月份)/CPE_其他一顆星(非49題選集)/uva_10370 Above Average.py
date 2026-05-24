15c8f5a46eaff6f0e156c48fb09ba5fe0093c0fd
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 01:46:47 +0800

    Update uva_10370 Above Average.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10370 Above Average.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10370 Above Average.py
@@ -1,18 +1,18 @@
 tc = int(input())
 for _ in range(tc):
 	seq = list(map(int, input().split()))
-	n = int(seq[0])
+	n = seq[0]
 	target = seq[1:]
-	
-	avg = sum(target) // n
-	target = sorted(target)[::-1]
+	target.sort()
+	avg = sum(target) / n
 	
 	count = 0
 	for num in target:
 		if num > avg:
 			count += 1
 		else:
-			break
+			continue
 	
 	ans = 100 * count / n
 	print(f"{ans:.3f}%")
+# 2026.05.25 二刷
