c9e06dcb0a106623c6be5a5a65802597f0ed6b30
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-24 20:09:13 +0800

    Update uva_10107 What is the Median?.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10107 What is the Median?.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10107 What is the Median?.py
@@ -1,16 +1,17 @@
 temp = []
 while True:
     try:
-        n = int(input())
+        num = int(input())
     except:
         break
 
-    temp.append(n)
+    temp.append(num)
     temp.sort()
+
     mid = len(temp) // 2
 
     if len(temp) % 2 == 0:
         print((temp[mid-1] + temp[mid]) // 2)
-        
     else:
         print(temp[mid])
+# 2026.05.24 二刷
