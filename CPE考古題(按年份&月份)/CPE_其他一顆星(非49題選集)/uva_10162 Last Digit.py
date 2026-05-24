659bc96c4eaccc7bc20b3a92974cb4b6cc5a4b40
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-24 20:48:47 +0800

    Update uva_10162 Last Digit.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10162 Last Digit.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10162 Last Digit.py
@@ -1,11 +1,14 @@
 # 先建表，因為存在100個循環之規律
 # 切記需要只留個位數去做計算，避免過多位數造成TLE
-table = [0] * 101
-for num in range(1, 101):
-	table[num] += ((num ** num) % 10 + table[num-1]) % 10
+MAX = 100
+table = [0] * (MAX+1)
+for i in range(1, MAX+1):
+    table[i] += (i**i + table[i-1]) % 10
 
 while True:
-	target = int(input())
-	if target == 0:
-		break
-	print(table[target % 100])
+    num = int(input())
+    if num == 0:
+        break
+    
+    print(table[num%100])
+# 2026.05.24 二刷
