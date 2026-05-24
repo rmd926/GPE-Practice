af118a82956eb302e163f5f93edf7349df369892
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 02:50:09 +0800

    Update uva_10433 Automorphic Numbers.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10433 Automorphic Numbers.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10433 Automorphic Numbers.py
@@ -5,14 +5,16 @@ while True:
         break
 
     num = int(target)
-
     if num == 0 or num == 1:
         print("Not an Automorphic number.")
         continue
 
-    square = str(num ** 2)
+    n = len(target)
 
-    if square[-len(target):] == target:
-        print(f"Automorphic number of {len(target)}-digit.")
+    square = num ** 2
+    if num == int(str(square)[-n:]):
+        print(f"Automorphic number of {n}-digit.")
     else:
         print("Not an Automorphic number.")
+
+# 2026.05.25 二刷，要注意輸入可能會有前導0的這種測資，因此input必須是字串格式，再用num去轉。另外 n-digit這種Output，n就是input的字串長度，和轉成num的長度不要混用。
