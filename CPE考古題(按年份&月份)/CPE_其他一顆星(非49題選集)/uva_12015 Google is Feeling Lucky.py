39db06e07251a7adfd8aa55237e6c2a0f538fa3e
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-26 02:05:27 +0800

    Update uva_12015 Google is Feeling Lucky.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_12015 Google is Feeling Lucky.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_12015 Google is Feeling Lucky.py
@@ -1,13 +1,15 @@
 tc = int(input())
 for t in range(tc):
-    lookup = {}
-    for _ in range(10):
-        link, score = map(str, input().split())
-        lookup[link] = int(score)
-    
-    max_score = max(lookup.values())
-    print(f"Case #{t+1}:")
+	lookup = {}
+	for _ in range(10):
+		link, score = map(str, input().split())
+		lookup[link] = int(score)
+	
+	print(f"Case #{t+1}:")
+	max_score = max(lookup.values())
+	
+	for key, value in lookup.items():
+		if value == max_score:
+			print(key)
 
-    for key, value in lookup.items():
-        if value == max_score:
-            print(key)
+# 2026.05.26 二刷
