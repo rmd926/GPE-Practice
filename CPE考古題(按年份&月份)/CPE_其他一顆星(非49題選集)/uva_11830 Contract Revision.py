891cf093376167769aae918be5a7e06ddfc0d54a
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-26 01:39:44 +0800

    Update uva_11830 Contract Revision.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11830 Contract Revision.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11830 Contract Revision.py
@@ -1,20 +1,22 @@
 while True:
     try:
-        D, N = map(int, input().split())
+        D, N = map(str, input().split())
     except:
         break
 
-    if D == N == 0:
-        break   
-    output = ""
-
-    for char in str(N):
-        if char == str(D):
+    if N == D == "0":
+        break
+    
+    ans = ""
+    for ch in N:
+        if ch in D:
             continue
         else:
-            output += char
-            
-    if output == "": # 會有完全顯示不出來的case -> 0
+            ans += ch
+    
+    if ans == "": # 會有完全顯示不出來的case -> 0
         print(0)
-    else: # 轉成int避免出現0開頭或是全0的case
-        print(int(output))
+    else:
+        print(int(ans))
+
+# 2026.05.26 二刷
