28b79f1db45032a03ac2a5fea6203cc9a24a51ac
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 16:48:39 +0800

    Update uva_10474 Where is the marble.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10474 Where is the marble.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10474 Where is the marble.py
@@ -1,26 +1,30 @@
 tc = 1
 while True:
-    N, Q = map(int, input().split())
-    if N == 0 and Q == 0:
+    try:
+        N, Q = map(int, input().split())
+    except:
         break
-    
-    target = []
+    if N == Q == 0:
+        break
+
+    print(f"CASE# {tc}:")
+
+    marble = []
     for _ in range(N):
         num = int(input())
-        target.append(num)
-    target.sort()
-    
+        marble.append(num)
+    marble.sort()
+
     query_list = []
     for _ in range(Q):
         query = int(input())
         query_list.append(query)
     
-    print(f"CASE# {tc}:")
-    
     for num in query_list:
-        if num in target:
-            print(f"{num} found at {target.index(num) + 1}")
+        if num in marble:
+            print(f"{num} found at {marble.index(num) + 1}")
         else:
             print(f"{num} not found")
     
     tc += 1
+# 2026.05.25 二刷
