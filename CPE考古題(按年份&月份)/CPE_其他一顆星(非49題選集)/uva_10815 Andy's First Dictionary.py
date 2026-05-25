f163c5953cd308e46d723ac7251a40f6d01b5221
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 18:55:59 +0800

    Update uva_10815 Andy's First Dictionary.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10815 Andy's First Dictionary.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10815 Andy's First Dictionary.py
@@ -1,27 +1,30 @@
 lookup = {}
 while True:
-    try:
-        line = input().lower()
-    except:
-        break
-
-    word = ""
-    for ch in line:
-        if ch.isalpha():
-            word += ch
-        else:
-            if word != "":
-                if word not in lookup:
-                    lookup[word] = 1
-                else:
-                    lookup[word] += 1
-                word = ""
-
-    if word != "":
-        if word not in lookup:
-            lookup[word] = 1
-        else:
-            lookup[word] += 1
-    
+	try:
+		line = input().lower()
+	except:
+		break
+	
+	word = ""
+	for ch in line:
+		if ch.isalpha():
+			word += ch
+		else:
+			if word != "":
+				if word in lookup:
+					lookup[word] += 1
+				else:
+					lookup[word] = 1
+				
+				word = ""
+	
+	if word != "":
+		if word in lookup:
+			lookup[word] += 1
+		else:
+			lookup[word] = 1
+	
 for key, value in sorted(lookup.items()):
-    print(key)
+	print(key)
+
+# 2026.05.25 二刷 line 21要注意
