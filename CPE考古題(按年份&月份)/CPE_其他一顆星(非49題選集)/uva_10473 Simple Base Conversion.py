9013da90a739d73fba571ffc7b84cce593c001af
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 16:21:29 +0800

    Update uva_10473 Simple Base Conversion.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10473 Simple Base Conversion.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10473 Simple Base Conversion.py
@@ -1,13 +1,14 @@
 while True:
     try:
-        target = input().strip()
-    except EOFError:
+        target = input()
+    except:
         break
-
+    
     if target == "-1":
         break
-
-    if target.startswith("0x") or target.startswith("0X"):
+    if target[0:2] == "0x" or target[0:2] == "0X":
         print(int(target, 16))
     else:
         print("0x" + hex(int(target))[2:].upper())
+
+# 2026.05.25 二刷
