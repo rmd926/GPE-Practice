7429e1cf961a9c3139ae47960baba1b7937a46e7
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 19:15:11 +0800

    Update uva_10919 Prerequisites?.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10919 Prerequisites?.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_10919 Prerequisites?.py
@@ -3,29 +3,30 @@ while True:
         k, m = map(int, input().split())
     except:
         break
-    
+    lookup = list(map(int, input().split()))
+
     if k == 0:
         break
 
-    lookup = list(map(int, input().split()))
     status = True
-    
     for _ in range(m):
-        count = 0
         seq = list(map(int, input().split()))
+        count = 0
 
         for num in lookup:
             if num in seq[2:]:
                 count += 1
             else:
                 continue
-
+            
         if count >= seq[1]:
             continue
         else:
             status = False
-
+    
     if status:
         print("yes")
     else:
         print("no")
+
+# 2026.05.25 二刷 細節要注意
