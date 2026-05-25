0bbe1daa55355c3d6543d20c2cab152bd9d50641
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-25 19:57:45 +0800

    Update uva_11192 Group Reverse.py

--- CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11192 Group Reverse.py
+++ CPE考古題(按年份&月份)/CPE_其他一顆星(非49題選集)/uva_11192 Group Reverse.py
@@ -1,17 +1,20 @@
 while True:
-    try:
-        G, target = map(str, input().split())
-    except:
-        break
-    
-    if G == "0":
-        break
+	try:
+		G, target = map(str, input().split())
+	except:
+		break
+	
+	if G == "0":
+		break
+		
+	n = len(target)
+	G = int(G)
+	group = len(target) // G
+	
+	ans = ""
+	for i in range(0, n+1, group):
+		ans += target[i: i+group][::-1]
+	
+	print(ans)
 
-    G = int(G)
-    n = len(target) // G
-
-    ans = ""
-    for i in range(0, len(target), n):
-        ans += target[i: i+n][::-1]
-    
-    print(ans)
+# 2026.05.25 二刷
