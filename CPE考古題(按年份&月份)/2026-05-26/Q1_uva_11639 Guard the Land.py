a92e168f688c8bf2e8458cc6054230fbe779e327
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-27 02:39:09 +0800

    Create Q1_uva_11639 Guard the Land.py

---
+++ CPE考古題(按年份&月份)/2026-05-26/Q1_uva_11639 Guard the Land.py
@@ -0,0 +1,16 @@
+tc = int(input())
+for t in range(tc):
+    x1, y1, x2, y2 = map(int, input().split())
+    x3, y3, x4, y4 = map(int, input().split())
+
+    strong = [max(x1, x3), max(y1, y3), min(x2, x4), min(y2, y4)]
+
+    if strong[2] > strong[0] and strong[3] > strong[1]:
+        strong_safe = (strong[2] - strong[0]) * (strong[3] - strong[1])
+    else:
+        strong_safe = 0
+
+    weak_safe = (x2 - x1) * (y2 - y1) + (x4 - x3) * (y4 - y3) - strong_safe * 2
+    unsafe = 10000 - weak_safe - strong_safe
+
+    print(f"Night {t+1}: {strong_safe} {weak_safe} {unsafe}")
