4e7f8201a4967b3231a67912ae81961bcc18df24
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-29 00:14:26 +0800

    Create GPE_10416 Last Digit.py

---
+++ GPE考古題(ref: GPE Helper)/GPE_10416 Last Digit.py
@@ -0,0 +1,13 @@
+# 先建表，因為存在100個循環之規律
+# 切記需要只留個位數去做計算，避免過多位數造成TLE
+MAX = 100
+table = [0] * (MAX+1)
+for i in range(1, MAX+1):
+    table[i] += (i**i + table[i-1]) % 10
+
+while True:
+    num = int(input())
+    if num == 0:
+        break
+    
+    print(table[num%100])
