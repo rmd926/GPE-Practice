8059045da3b5f21f4e647745cc465551cccb5371
Barry <75135224+rmd926@users.noreply.github.com>
2026-05-29 18:07:17 +0800

    Update GPE_2015-01 Missing Numbers.py

--- GPE考古題(ref: GPE Helper)/GPE_2015-01 Missing Numbers.py
+++ GPE考古題(ref: GPE Helper)/GPE_2015-01 Missing Numbers.py
@@ -1,3 +1,24 @@
+# v1
+import sys
+
+while True:
+    line = sys.stdin.readline()
+
+    if line == "":
+        break
+
+    M, N = map(int, line.split())
+
+    last_sum = sum(map(int, sys.stdin.readline().split()))
+
+    for _ in range(M - 1):
+        cur_sum = sum(map(int, sys.stdin.readline().split()))
+
+        print(last_sum - cur_sum)
+
+        last_sum = cur_sum
+
+# v2
 while True:
     try:
         M, N = map(int, input().split())
